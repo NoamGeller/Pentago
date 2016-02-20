fabee2ec8e1420b5a5fd6f7f7e7aba864c37dced
NoamGeller <noam.geller@mail.huji.ac.il>
2016-02-20 19:36:55 +0200

    AlphaBetaAgent skeleton
    (accidently included in the previous commit)

--- AlphaBetaAgent.java
+++ AlphaBetaAgent.java
@@ -1,10 +1,20 @@
 
 public class AlphaBetaAgent extends Agent {
+	
+	private int maxDepth;
+
+	public AlphaBetaAgent(int maxDepth) {
+		this.maxDepth = maxDepth;
+	}
+	
+	private Move alphaBetaPruning(Square[][] board, int depth, double alpha, double beta)
+	{
+		return null;
+	}
 
 	@Override
-	public Move getMove(Move[] moves, Square[][] board) {
-		// TODO Auto-generated method stub
-		return null;
+	public Move getMove(Square[][] board) {
+		return alphaBetaPruning(board, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
 	}
 
 	@Override
