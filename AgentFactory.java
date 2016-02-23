fdcc6ec422076d5512166383b857c914cad2d969
NoamGeller <noam.geller@mail.huji.ac.il>
2016-02-23 12:31:36 +0200

    factory has human agent and documentation

--- AgentFactory.java
+++ AgentFactory.java
@@ -1,11 +1,24 @@
 
 public class AgentFactory {
-	public static Agent getAgent(String name, int color, int[] details)
+	/** 
+	 * @param name AlphaBeta or Brain, for now
+	 * @param details depth (0 for human), color (-1 or 1), etc.
+	 * @return The requested Agent, or null for human.
+	 */
+	public static Agent getAgent(String name, int[] details)
 	{
+		if (details[0] == 0)
+		{
+			return null;
+		}
 		if (name.equals("AlphaBeta"))
 		{
-			return new AlphaBetaAgent(details[0], color);
+			return new AlphaBetaAgent(details[0], details[1]);
 		}
-		return new BrainAgent(color, details[0], details[1]);
+		if (name.equals("Brain"))
+		{
+			return new BrainAgent(details[1], details[0], details[2]);
+		}
+		return null;
 	}
 }
