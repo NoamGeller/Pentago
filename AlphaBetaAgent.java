5a435b1c9ae5300a586dc25bddb2a8e5ba61e291
NoamGeller <noam.geller@mail.huji.ac.il>
2016-02-29 18:39:35 +0200

    AlphaBeta basic evaluation
    (ignoring diagonals)

--- AlphaBetaAgent.java
+++ AlphaBetaAgent.java
@@ -13,14 +13,24 @@ public class AlphaBetaAgent extends Agent {
 	
 	private Move alphaBetaPruning(Square[][] board, int depth, int alpha, int beta)
 	{
-		Move bestMove = null;
-		if (depth == 0 || Brain.isWin(board, 1) || Brain.isWin(board, -1))
+		Move bestMove = new Move();
+		boolean thisWin = Brain.isWin(board, color);
+		boolean otherWin = Brain.isWin(board, -color);
+		if (otherWin && !thisWin)
 		{
-			bestMove = new Move();
+			bestMove.setGrade(Integer.MIN_VALUE);
+		}
+		else if (!otherWin && thisWin)
+		{
+			bestMove.setGrade(Integer.MAX_VALUE);
+		}
+		else if (depth == 0 || (otherWin && thisWin))
+		{
 			bestMove.setGrade(evaluation(board));
 		}
 		else
 		{
+			bestMove = null;
 			List<Move> moves = Brain.getMoves(board);
 			int currentColor = this.color * (depth % 2 == 0 ? 1 : -1);
 			for (Move move : moves)
@@ -57,8 +67,27 @@ public class AlphaBetaAgent extends Agent {
 	@Override
 	int evaluation(Square[][] board) 
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		for (int i = 0; i < board.length; i++)
+		{
+			int[] stoneCounters = {0, 0, 0, 0, 0, 0};
+			for (int j = 0; j < board[i].length; j++)
+			{
+				stoneCounters[0] += Math.max(0, color*board[i][j].getColor());
+				stoneCounters[1] += Math.max(0, color*board[j][i].getColor());
+				stoneCounters[3] += Math.max(0, -color*board[i][j].getColor());
+				stoneCounters[4] += Math.max(0, -color*board[j][i].getColor());
+			}
+			result += Math.pow(10, stoneCounters[0]-1) + Math.pow(10, stoneCounters[1]-1);
+			result -= Math.pow(10, stoneCounters[2]-1) + Math.pow(10, stoneCounters[3]-1);
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			int x = i < 2 ? 1 : 4;
+			int y = i % 2 == 0 ? 1 : 4;
+			result += 5*board[x][y].getColor();
+		}
+		return result;
 	}
 
 	@Override
